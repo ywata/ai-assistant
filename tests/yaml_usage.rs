5a0eddae6a776b6e50a3585a0f123c0c962dcb09
Yasu Watanabe <ywata@protonmail.com>
2024-02-14 22:32:31 +0900

    style: apply rustfmat

--- tests/yaml_usage.rs
+++ tests/yaml_usage.rs
@@ -1,30 +1,28 @@
-
-
-use serde::{Serialize, Deserialize};
-use serde_yaml::{Value};
+use serde::{Deserialize, Serialize};
+use serde_yaml::Value;
 
 #[derive(Debug, PartialEq, Serialize, Deserialize)]
-struct OpenAIConfig{
-    token: String
+struct OpenAIConfig {
+    token: String,
 }
 
 #[test]
-fn test_serde_yaml_from_str(){
+fn test_serde_yaml_from_str() {
     let yaml_string = r#"
     openai:
       token: "The token"
     "#;
-    let map : Result<Value, serde_yaml::Error> = serde_yaml::from_str(yaml_string);
+    let map: Result<Value, serde_yaml::Error> = serde_yaml::from_str(yaml_string);
     assert!(map.is_ok());
     let val = map.unwrap();
     assert_eq!(val["openai"]["token"], "The token");
 }
 
 #[test]
-fn test_serde_yaml_from_file(){
+fn test_serde_yaml_from_file() {
     let yaml_str = r#"
     foo:
       bar:1
     "#;
-    let _value = serde_yaml::from_str(yaml_str).map(|_v:Value| ());
+    let _value = serde_yaml::from_str(yaml_str).map(|_v: Value| ());
 }

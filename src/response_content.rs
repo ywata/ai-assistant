33a5b0e49b2ebb835f3b3de0af33e10bbe33daa2
Yasu Watanabe <ywata@protonmail.com>
2024-03-26 05:36:30 +0900

    refactor: move test for response_content

--- src/response_content.rs
+++ src/response_content.rs
@@ -60,3 +60,108 @@ pub fn get_content(contents: Vec<Mark>) -> Option<Mark> {
     }
     res
 }
+
+mod test {
+    use super::*;
+    use regex::Regex;
+    #[test]
+    fn test_split_mark_only() {
+        let input = r#"```start
+```"#
+            .to_string();
+        let markers = vec!["```(start)".to_string(), "```".to_string()];
+        let rex_markers: Result<Vec<_>, _> = markers.iter().map(|s| Regex::new(s)).collect();
+
+        let res = split_code(&input, &rex_markers.unwrap());
+        assert_eq!(res.len(), 3);
+        assert_eq!(
+            res.first(),
+            Some(&Mark::Marker {
+                text: "```start".to_string(),
+                lang: Some("start".to_string())
+            })
+        );
+        assert_eq!(
+            res.get(1),
+            Some(&Mark::Content {
+                text: "\n".to_string(),
+                lang: Some("start".to_string())
+            })
+        );
+        assert_eq!(
+            res.get(2),
+            Some(&Mark::Marker {
+                text: "```".to_string(),
+                lang: None
+            })
+        );
+    }
+    #[test]
+    fn test_split_mark_backquotes() {
+        let input = r#"```start
+asdf
+```"#
+            .to_string();
+        let markers = vec!["```start```".to_string()];
+        let rex_markers: Result<Vec<_>, _> = markers.iter().map(|s| Regex::new(s)).collect();
+
+        let res = split_code(&input, &rex_markers.unwrap());
+        assert_eq!(res.len(), 1);
+        assert_eq!(
+            res.first(),
+            Some(&Mark::Content {
+                text: "```start\nasdf\n```".to_string(),
+                lang: None
+            })
+        );
+    }
+
+    #[test]
+    fn test_split_mark_and_content() {
+        let input = r#"asdf
+```start
+hjklm
+```
+xyzw
+"#
+        .to_string();
+        let markers = vec!["```(start)".to_string(), "```".to_string()];
+        let rex_markers: Result<Vec<_>, _> = markers.iter().map(|s| Regex::new(s)).collect();
+
+        let res = split_code(&input, &rex_markers.unwrap());
+        assert_eq!(res.len(), 5);
+        assert_eq!(
+            res.first(),
+            Some(&Mark::Content {
+                text: "asdf\n".to_string(),
+                lang: None
+            })
+        );
+        assert_eq!(
+            res.get(1),
+            Some(&Mark::Marker {
+                text: "```start".to_string(),
+                lang: Some("start".to_string())
+            })
+        );
+        //assert_eq!(res.get(2), Some(&Mark::Content{text:"\nhjklm\n".to_string(), lang: Some("start".to_string())}));
+        //assert_eq!(res.get(3), Some(&Mark::Marker{text:"```".to_string(), lang: None}));
+        //assert_eq!(res.get(4), Some(&Mark::Content{text:"\nxyzw\n".to_string(), lang: None}));
+    }
+    #[test]
+    fn test_regex() {
+        let rex_str = r#"^([a-zA-Z]+)[0-9]+"#;
+        let rex = Regex::new(rex_str).unwrap();
+
+        let input = r#"abcd123x"#;
+
+        if let Some(m1) = rex.captures(input) {
+            let g0 = m1.get(0).unwrap().as_str();
+            let g1 = m1.get(1).unwrap().as_str();
+            assert_eq!(g0, "abcd123");
+            assert_eq!(g1, "abcd");
+            return;
+        }
+        assert_eq!(true, false);
+    }
+}
